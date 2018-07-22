a024c5080fbc962d8201847c2b2f7a9c0b7b9cd4
Vaibhav Bali <vbali@internode.on.net>
2018-07-22 15:07:20 +1000

    Stack and Queues

--- StacksAndQueues/Queues.cs
+++ StacksAndQueues/Queues.cs
@@ -1,10 +1,121 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace StacksAndQueues
 {
-    class Queues
+    public class Queues<T>
     {
+       private int head = -1;
+       private int tail = 0;
+       private T[] backingStore;
+
+       public Queues(int size = 0)
+       {
+          if (size < 4)
+          {
+             size = 4;
+          }
+
+          InitialSize = size;
+          backingStore = new T[InitialSize];
+       }
+
+       public void Enqueue(T item)
+       {
+          //We have depleted the backing store
+          if (Count == backingStore.Length || head == backingStore.Length)
+          {
+             //we still have some storage
+             if (Count < backingStore.Length)
+             {
+                head = -1;
+                return;
+             }
+
+             IncreaseStorage();
+          }
+
+         backingStore[++head] = item;
+          ++Count;
+
+          
+       }
+
+       private void IncreaseStorage()
+       {
+          var temp = new T[backingStore.Length * 2];
+          //check if wrapped
+          if (head <= tail)
+          {
+             var totalElements = backingStore.Length - tail - 1;
+             Array.Copy(backingStore, tail, temp, 0, totalElements);
+             Array.Copy(backingStore, 0, temp, totalElements + 1, head);
+             tail = 0;
+             head = Count - 1;
+          }
+          else
+          {
+             Array.Copy(backingStore, 0, temp, 0, Count);
+          }
+
+          backingStore = temp;
+       }
+
+       public T Dequeue()
+       {
+          var itemToReturn = Peek();
+          backingStore[tail++] = default(T);
+          --Count;
+
+          if (tail >= backingStore.Length)
+          {
+             tail = 0;
+          }
+
+          if (backingStore.Length > 4 && Count < backingStore.Length / 3)
+          {
+             DecreaseSize();
+          }
+
+         return itemToReturn;
+       }
+
+       private void DecreaseSize()
+       {
+         var temp = new T[backingStore.Length * 1/2];
+          if (tail > head)
+          {
+             throw new ApplicationException("Edge case");
+          }
+
+          if (head == tail)
+          {
+             temp[0] = backingStore[head];
+             tail = 0;
+             head = 0;
+          }else
+          {
+             var totalElements = head - tail;
+             Array.Copy(backingStore, tail, temp, 0, totalElements);
+             Array.Copy(backingStore, 0, temp, totalElements + 1, head);
+             tail = 0;
+             
+          }
+          
+
+          backingStore = temp;
+      }
+
+       public T Peek()
+       {
+          if (Count == 0)
+          {
+             throw new InvalidOperationException();
+          }
+
+          return backingStore[tail];
+       }
+
+       public int Count { get; private set; }
+       public int InitialSize { get; set; }
     }
 }
--- StacksAndQueues/Stacks.cs
+++ StacksAndQueues/Stacks.cs
@@ -1,8 +1,93 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace StacksAndQueues
 {
-    public class Stacks
-    {
-    }
+   public class Stacks<T> : IEnumerable<T>
+   {
+      private T[] backingStore;
+      private int head = -1;
+      public int Count { get; private set; }
+      public Stacks(int size)
+      {
+         if (size < 4)
+         {
+            size = 4;
+         }
+
+         backingStore = new T[size];
+      }
+
+      public void Push(T item)
+      {
+         backingStore[++head] = item;
+         if (head > backingStore.Length)
+         {
+            IncreaseSize();
+         }
+      }
+
+      private void IncreaseSize()
+      {
+         if (head == 0)
+         {
+            throw new InvalidOperationException();
+         }
+         var temp = new T[backingStore.Length * 2];
+         Array.Copy(backingStore, 0, temp, 0, head - 1);
+         backingStore = temp;
+      }
+
+      public T Pop()
+      {
+         if (Count == 0)
+         {
+            throw new InvalidOperationException();
+         }
+
+         //if only 1/3 is filled then decrease the size
+         if (backingStore.Length > 4 && head < backingStore.Length / 3)
+         {
+            DecreaseSize();
+         }
+
+         return backingStore[--head];
+      }
+
+      public T Peek()
+      {
+         if (Count > 0)
+         {
+            return backingStore[head - 1];
+         }
+
+         throw new InvalidOperationException();
+      }
+
+      private void DecreaseSize()
+      {
+         if (head == 0)
+         {
+            throw new InvalidOperationException();
+         }
+         var temp = new T[backingStore.Length / 2];
+         Array.Copy(backingStore, 0, temp, 0, head - 1);
+         backingStore = temp;
+      }
+
+
+      public IEnumerator<T> GetEnumerator()
+      {
+         for (int i = head - 1; i >= 0 ; i--)
+         {
+            yield return backingStore[i];
+         }
+      }
+
+      IEnumerator IEnumerable.GetEnumerator()
+      {
+         return GetEnumerator();
+      }
+   }
 }
